b7bf892d807f0d207aa184717d1cbf1253f6828b
Philip Taffet <ptaffet@rice.edu>
2016-10-29 03:54:57 +0100

    Fixed many things, but still probably won't compile.

--- prototype_sketch/GuessParameters2/GuessParameters2.ino
+++ prototype_sketch/GuessParameters2/GuessParameters2.ino
@@ -1,18 +1,34 @@
-#include <FHT.h>
-#include "data.h"
+#define PARAM_LEARN_SIZE 8192 //The number of points to use in parameter learning
 
+
+#define SAMPLE_RATE 1000
 #define MIN_HEARTRATE 50 //BPM
 #define MAX_HEARTRATE 150 //BPM
 
-//need data to get inserted here as fht_input[]
-short fht_lin_out[];
+// The we expect two rising, falling edges per beat, but the 3 gives us padding
+#define MAX_EDGES (PARAM_LEARN_SIZE * 3 * MAX_HEARTRATE) / (SAMPLE_RATE * 60)
+
+#if FFT
+	#define FHT_N PARAM_LEARN_SIZE
+
+	#include <FHT.h>
+	//need data to get inserted here as fht_input[]
+	short fht_lin_out[NHT_N/2];
+#endif
+
+#include "data.h"
+
+short data[PARAM_LEARN_SIZE];
+bool thresholded[PARAM_LEARN_SIZE];
+short rising_edges[MAX_EDGES];
+short falling_edges[MAX_EDGES];
 
 //Struct that holds vc, ac, and SamplesHB
 struct guessed_param {
   short ac;
   short vc;
   short samplesHB;
-}
+};
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,7 +42,7 @@ void loop() {
 
 void GuessParameters2() { 
   short heartbeats = FindHeartRate();
-  guessed_param.samplesHB = floor((sizeof(fht_input)/sizeof(short))/heartbeats);
+  guessed_param.samplesHB = PARAM_LEARN_SIZE/heartbeats;
 
   short v_cutoffs[] = {0, 0};
   v_cutoffs[0] = BinarySearch(fht_input, heartbeats+2, 1);
@@ -34,43 +50,54 @@ void GuessParameters2() {
   
   guessed_param.vc = v_cutoffs[1] >> 1 + v_cutoffs[2] >> 1; //.5*v_cutoffs[1] + .5*v_cutoffs[2] 
 
-  short ndata[];
-  memcpy(ndata, fht_input, (sizeof(fht_input)/sizeof(short))); //copy array
 
   short t_blank = 30;
   short l_blank = 30;
-  short rising_edges = CountPeaks(); //Change this line
-  //    rising_edges = find(rising_edges);
-  //  falling_edges = find(falling_edges);
-  //  for i = 1:length(rising_edges)
-  //      ndata((rising_edges(i)-t_blank):(falling_edges(i)+l_blank)) = 0;
-  //  end
+
+  for (short i = 0; i < PARAM_LEARN_SIZE; i++)
+  	thresholded[i] = data[i] > guessed_param.vc;
+
+  short n_edges = CountPeaks(thresholded, rising_edges, falling_edges, PARAM_LEARN_SIZE, MAX_EDGES);
+  for (short peak = 0; peak < n_edges; peak++)
+  {
+  	short start_idx = max(rising_edges[peak] - t_blank, 0);
+	short end_idx = min(falling_edges[peak] + l_blank, PARAM_LEARN_SIZE - 1);
+	for (short j = start_idx; j < end_idx; j++)
+		data[j] = 0;
+  }
 
   short a_cutoffs[] = {0, 0};
-  a_cutoffs[0] = BinarySearch(ndata, heartbeats+2, 1);
-  a_cutoffs[1] = BinarySearch(ndata, heartbeats-2, 0);
-  guessed_param.ac = .7*a_cutoffs[0] + .3*a_cutoffs[1];
+  a_cutoffs[0] = BinarySearch(data, heartbeats+2, 1);
+  a_cutoffs[1] = BinarySearch(data, heartbeats-2, 0);
+  guessed_param.ac = (7*a_cutoffs[0] + 3*a_cutoffs[1])/10;
   return;
 }
+// above_th is basically scratch memory used by this function
+// both data and above_th should be of the specified length 
+short BinarySearch(short* data, short length, bool* above_th, short expected, short hh) {
+  short low = 0;
+  short high = 0;
 
-short BinarySearch(short data, short expected, short hh) {
-  short low = min(data);
-  short high = max(data);
+	for (int i = 0; i < length; i++)
+	{
+		low = min(low, data[i]);
+		high = max(high, data[i]);
+	}
+
+
   short mid;
   short count;
-  //Assuming 10 s of data
-  bool above_th[10000];
   
   for(short i = 0; i <= 9; i++){
     mid = (low+high) >> 1; //(Low + High) / 2
-    for(short i = 0; i < (sizeof(fht_input)/sizeof(short)); i++) {
+    for(short i = 0; i < length; i++) {
       if (fht_input > mid){
         above_th[i] = true;
       } else {
         above_th[i] = false;
       }
     }
-    count = CountPeaks(); //NEED TO CHANGE THIS LINE
+    count = CountPeaks(above_th, rising_edges, falling_edges, PARAM_LEARN_SIZE, MAX_EDGES); //NEED TO CHANGE THIS LINE
 
     if(count > expected) || ((count == expected) && (hh == 0)){
       low = mid;
@@ -101,7 +128,7 @@ const short filter[] = {3480, 3481, 3482, 3483, 3484, 3485, 3486, 3486, 3486, 34
 // fills in edges[i] with a 1 if i is a rising edge, 2 if i is a falling edge 
 // all arrays should be allocated and of size length
 // TODO: really we want to return the indices of the peaks, which is less data, but is variable sized
-short CountPeaks(bool* th_data, char* edges, short length_data)
+short CountPeaks(bool* th_data, short* rising_edges, short* falling_edges, short length_data, short max_edges)
 {
   for(short i = 0; i < length_data; i++)
   {
@@ -109,78 +136,93 @@ short CountPeaks(bool* th_data, char* edges, short length_data)
     // Definitely need to break if we exceed the cutoff or we risk overflowing
     for(short j = 0; j < FILTER_LENGTH && i-j >= 0 && acc < TH_CUTOFF; j++)
     {
-      acc += filter[j] * th_data[i-j];
+	  if (th_data[i-j] & 1)
+		  acc += filter[j] * th_data[i-j];
     }
-    // Temporarily store in falling_edges so we don't need more RAM
-    edges[i] = acc >= TH_CUTOFF;
+    // Temporarily store in higher bits of th_data so we don't need more RAM
+    th_data[i] |= (acc >= TH_CUTOFF) << 1;
   }
   for(short i = 0; i < length_data; i++)
   {
-    th_data[i] = edges[i];
-    edges[i] = 0;
+    th_data[i] >>= 1; // Now move that higher bit back to bit 0
   }
   bool high = false;
   short rising_count = 0;
   short last_rising_index = 0;
-  for(short i = 0; i < length_data; i++)
-  {
-    if (th_data[i] && !high)
-    {
-      rising_count++;
-      last_rising_index = i;
-      high = true;
-      edges[i] = RISING_EDGE;
-    }
-    else if (!th_data[i] && high)
-    {
-      high = false;
-      rising_count += (i - last_rising_index)/SAMPLES_PER_ADDTL_HEARTBEAT;
-      edges[i] = FALLING_EDGE;
-    }
-  }
-  // Handle the case where the data ends while high:
-  if (high)
-  {
-    rising_count += (length_data - last_rising_index)/SAMPLES_PER_ADDTL_HEARTBEAT;
-    high = false;
-  }
-  return rising_count;
+
+	short ri = 0, fi = 0; // current positions in rising and falling edges arrays
+
+	for(short i = 0; i < length_data; i++)
+	{
+		if (th_data[i] && !high)
+		{
+			rising_count++;
+			last_rising_index = i;
+			high = true;
+
+			if (ri < max_edges) 
+			{
+				rising_edges[ri++] = i;
+			}
+		}
+		else if (!th_data[i] && high)
+		{
+			high = false;
+			rising_count += (i - last_rising_index)/SAMPLES_PER_ADDTL_HEARTBEAT;
+			
+			if (fi < max_edges)
+				falling_edges[fi++] = i;
+		}
+	}
+	// Handle the case where the data ends while high:
+	if (high)
+	{
+		rising_count += (length_data - last_rising_index)/SAMPLES_PER_ADDTL_HEARTBEAT;
+		high = false;
+		falling_edges[fi] = length_data - 1;
+	}
+	return rising_count;
 }
 
-
+// Returns the expected number of beats in data
 short FindHeartRate() {
-  //Explanation of function goes here
-  short sample_rate = 1000;
-  //Use FHT library. Assumes the data points are in SRAM and the final output is kept in fht_input[], 
-  //with the 0 -> N/2 bins having the sum of the real and imaginary components, 
-  //and the N/2 -> N having the difference between the real and imaginary components. 
-  //these two parts need to be squared and summed to get the overall magnitude at a particular frequency. 
-  //as a result, you will have to run one of the magnitude functions to get useful data from these bins.
-  fht_run();
-  //This gives the magnitude of each bin in the FHT. It sums the squares of the imaginary and real, 
-  //and then takes the square root. It uses a lookup table to calculate the square root, so it has limited precision. 
-  //You can think of it as an 8b value times a 4b exponent. It covers the full 16b range, 
-  //but only has 8b of precision at any point in that range. The data is taken in from fht_input[] and returned in fht_lin_out[]. 
-  //The values are in sequential order, and there are only N/2 values total, as the FHT returns redundant data.
-  fht_mag_lin();
-  short data_length = sizeof(fht_input) / sizeof(short);
-  short rescale = data_length / (sample_rate * 60);
-  short min_idx = MIN_HEARTRATE * rescale;
-  short max_idx = MAX_HEARTRATE * rescale;
-  
-  //Find index of max value of heartbeat data
-  short hr_idx = 0;
-  short hr_val = fht_lin_out[hr_idx];
-  for (i = 0; i < (sizeof(fht_lin_out) / sizeof(short)); ++i)
-  {
-      if (fht_lin_out[i] > hr_val)
-      {
-        hr_val = fht_lin_out[i];
-        hr_idx = i;
-      }
-  }
-  hr_idx = hr_idx - 1 + min_idx;
-  short heartbeats = hr_idx;
-  return heartbeats;
+#if FFT
+	short sample_rate = 1000;
+	//Use FHT library. Assumes the data points are in SRAM and the final output is kept in fht_input[], 
+	//with the 0 -> N/2 bins having the sum of the real and imaginary components, 
+	//and the N/2 -> N having the difference between the real and imaginary components. 
+	//these two parts need to be squared and summed to get the overall magnitude at a particular frequency. 
+	//as a result, you will have to run one of the magnitude functions to get useful data from these bins.
+	fht_run();
+	//This gives the magnitude of each bin in the FHT. It sums the squares of the imaginary and real, 
+	//and then takes the square root. It uses a lookup table to calculate the square root, so it has limited precision. 
+	//You can think of it as an 8b value times a 4b exponent. It covers the full 16b range, 
+	//but only has 8b of precision at any point in that range. The data is taken in from fht_input[] and returned in fht_lin_out[]. 
+	//The values are in sequential order, and there are only N/2 values total, as the FHT returns redundant data.
+	fht_mag_lin();
+	short data_length = sizeof(fht_input) / sizeof(short);
+	short rescale = data_length / (sample_rate * 60);
+	short min_idx = MIN_HEARTRATE * rescale;
+	short max_idx = MAX_HEARTRATE * rescale;
+
+	//Find index of max value of heartbeat data
+	short hr_idx = 0;
+	short hr_val = fht_lin_out[hr_idx];
+	for (i = 0; i < (sizeof(fht_lin_out) / sizeof(short)); ++i)
+	{
+		if (fht_lin_out[i] > hr_val)
+		{
+			hr_val = fht_lin_out[i];
+			hr_idx = i;
+		}
+	}
+	hr_idx = hr_idx - 1 + min_idx;
+	short heartbeats = hr_idx;
+	return heartbeats;
+#else
+	const int HARDCODED_BPM = 100;
+	return PARAM_LEARN_SIZE * HARDCODED_BPM / (60* SAMPLE_RATE );
+#endif
+
 }
 
