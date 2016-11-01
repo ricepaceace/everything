358030f78d9128dc885826eb538aa5c2d7ea1be7
ricepaceace <ricepaceace@gmail.com>
2016-11-01 11:05:29 -0500

    Add files via upload

--- multisite_detect.m
+++ multisite_detect.m
@@ -1,52 +1,64 @@
 % main script for running detection algorithms on test data
 addpath('test_data/sept29_2016_test/')
+addpath('test_data/PhisioBank_iaf/')
 
 % each .mat file contains a struct for data from a test
 % currently, each struct has two fields:
 % sampling_rate: the sampling rate of the data in Hz
 % data: a NxC matrix where each column represents a channel and each row
 %       represents a time point
-numChannels = 17;
-N = 6;
-fig_offset = 0;
 
-load('NormalSinusRhythm_struct.mat')
-load('Pacingfromchipapprox120bpmxmA_struct.mat')
-load('PacingfromMedtronic120bpm2mA_struct.mat')
-s = PacingfromMedtronic120bpm2mA_struct;
+
+%s = load('NormalSinusRhythm_struct.mat');
+%s = load('Pacingfromchipapprox120bpmxmA_struct.mat');
+%s = load('PacingfromMedtronic120bpm2mA_struct.mat');
+s = load('iaf2_struct.mat'); %number ranges from 1-8 for different patients
+
 sampling_rate = s.sampling_rate;
 data = s.data;
+numChannels = size(data,1);
 begin_time = 0.0;
-end_time = 10; %second
-%data = data(begin_time*sampling_rate+1:end_time*sampling_rate+1,:);
+end_time = 30; %second
+data = data(begin_time*sampling_rate+1:end_time*sampling_rate+1,:);
 
-%%% plotting data from multiple channels
-for f = 1:ceil(numChannels/N)
-    figure(f+fig_offset)
-    clf
-    subplot(N,1,1)
-    for i = (1:N)
-        if i+(f-1)*N > numChannels
-            break
-        end
-        subplot(N,1,i)
-        plot((0:length(data)-1)/sampling_rate,data(:,i+(f-1)*N))
-        ylabel(i+(f-1)*N)
-        set(gca,'XTick',[]);
-    end
-end
+% run on sum of all channels
+[a,v,samplesHB]=GuessParameters2(sum(data,2));
+%figure; hold on; plot(data,'b'); plot(a*(data>a), 'or');plot(v*(data>v).*(data<a), 'xp');
 
-figure(4+fig_offset)
-clf
-plot((0:length(data)-1)/sampling_rate,sum(data,2))
+detection.v_thresh = a;
+detection.v_length = 3;
+detection.a_thresh = v;
+detection.a_length = 8;
+[vind, aind] = one_chamber_peak_finder(detection, sum(data,2));
 
-detection.v_thresh = 1e4;
-detection.v_length = ceil(0.005*sampling_rate);
-detection.a_thresh = 1300;
-detection.a_length = ceil(0.02*sampling_rate);
+figure; hold on;
+plot(data,'b');
+plot(aind,  detection.a_thresh, 'xg');
+plot(vind,  detection.v_thresh, 'or');
+title('sum')
 
-[peak_indices peak_types] = one_chamber_peak_finder(detection,sum(data,2));
-hold on
-plot(peak_indices/sampling_rate, (peak_types=='v')*detection.v_thresh+(peak_types=='a')*detection.a_thresh,'*')
-
+%%% run on each channel individually
+% ainds = zeros(17, 1);
+% vinds = zeros(17, 1);
+% for i=1:size(NormalSinusRhythm_struct.data,2)
+%     data=NormalSinusRhythm_struct.data(:,i);
+%     [a,v,samplesHB]=GuessParameters2(data);
+%     %figure; hold on; plot(data,'b'); plot(a*(data>a), 'or');plot(v*(data>v).*(data<a), 'xg');
+% 
+%     detection.v_thresh = a;
+%     detection.v_length = 3;
+%     detection.a_thresh = v;
+%     detection.a_length = 8;
+%     [vind, aind] = one_chamber_peak_finder(detection, data);
+%     
+%     figure; hold on;
+%     plot(data,'b');
+%     plot(aind,  detection.a_thresh, 'xg');
+%     plot(vind,  detection.v_thresh, 'or');
+%     
+%     ainds(i) = length(aind);
+%     vinds(i) = length(vind);
+% end
+% ainds
+% vinds
 
