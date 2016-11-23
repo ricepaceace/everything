16a4b272138f9d1b8338b4068014a43839e555fe
ricepaceace <ricepaceace@gmail.com>
2016-11-22 18:24:36 -0600

    Add files via upload
    added functionality of checking both + an - data

--- GuessParameters2.m
+++ GuessParameters2.m
@@ -1,71 +1,85 @@
-function [ vc, ac ] = GuessParameters2( data ,v_length, a_length)
+function [ vc, ac, vflip, aflip ] = GuessParameters2( data, v_length, a_length)
 %GUESSPARAMETERS2 Summary of this function goes here
 %   Detailed explanation goes here
  
     %samplesHB = floor(length(data)/heartbeats);
     visualize_bs = true;% BINARY SEARCH VISUALIZATION
-     
+    
     if visualize_bs
-        figure(99)
-        hold on
-        xs = linspace(min(data), max(data), 200);
-        ys=zeros(200,1);
-        for i = 1:200
-            [beats, ~, ~] = CountPeaks(data > xs(i), v_length);
-            ys(i) = beats;
+        for vflip = [+1, -1]
+            figure(99)
+            hold on
+            xs = linspace(0, max(vflip*data), 200);
+            ys=zeros(200,1);
+            for i = 1:200
+                [beats, ~, ~] = CountPeaks(vflip*data > xs(i), v_length);
+                ys(i) = beats;
+            end
+            plot(vflip*xs, ys);
         end
-        plot(xs, ys);
     end
     
-    
-    
-    v_cutoffs = BinarySearch(data, v_length,0,max(data));
+    v_cutoffs = BinarySearch(data, v_length);
+    v_cutoffs_neg = BinarySearch(-data, v_length);
+    if (v_cutoffs_neg(2)-v_cutoffs_neg(1)<v_cutoffs(2)-v_cutoffs(1))
+        vflip = 1;
+    else
+        vflip = -1;
+        v_cutoffs = v_cutoffs_neg;
+    end
     vc = sum([.5, .5] .* v_cutoffs);
+    
     
     if visualize_bs
         heartbeats = FindHeartRate(data);
-        plot(v_cutoffs, [heartbeats, heartbeats], 'bx');
+        plot(vflip*v_cutoffs, [heartbeats, heartbeats], 'bx');
     end
 
     ndata = data;
     
-    t_blank = 20;
-    l_blank = 20;
-    [~, rising_edges, falling_edges] = CountPeaks(data > vc, v_length);
+    t_blank = 50;
+    l_blank = 50;
+    [~, rising_edges, falling_edges] = CountPeaks(vflip*data > vc, v_length);
     for i = 1:length(rising_edges)
-        ndata((rising_edges(i)-t_blank):(falling_edges(i)+l_blank)) = min(data);
+        ndata((rising_edges(i)-t_blank):(falling_edges(i)+l_blank)) = 0;
     end
 %     ndata = abs(ndata);
 %     figure
 %     plot(ndata)
     
     if visualize_bs
-        xs = linspace(min(ndata), max(ndata), 200);
-        ys=zeros(200,1);
-        for i = 1:200
-            [beats, ~, ~] = CountPeaks(ndata > xs(i), a_length);
-            ys(i) = beats;
+        for aflip = [+1, -1]
+            xs = linspace(0, max(aflip*ndata), 200);
+            ys=zeros(200,1);
+            for i = 1:200
+                [beats, ~, ~] = CountPeaks(aflip*ndata > xs(i), a_length);
+                ys(i) = beats;
+            end
+            plot(aflip*xs, ys, 'k');
         end
-         plot(xs, ys, 'k');
     end
 
-    a_cutoffs =  BinarySearch(ndata, a_length, 0, max(ndata));
-    
-    ac = sum([.7, .3] .* a_cutoffs);
-    if ac==0
-        vc = 0;
+    a_cutoffs =  BinarySearch(ndata, a_length);
+    a_cutoffs_neg =  BinarySearch(-ndata, a_length);
+    if (a_cutoffs_neg(2)-a_cutoffs_neg(1)<a_cutoffs(2)+a_cutoffs(1))
+        aflip = 1;
+    else
+        a_cutoffs = a_cutoffs_neg;
+        aflip = -1;
     end
+    ac = sum([.7, .3] .* a_cutoffs);
     
-     if visualize_bs
-        plot(a_cutoffs, [heartbeats, heartbeats], 'ro');
-     end
+    if visualize_bs
+        plot(aflip*a_cutoffs, [heartbeats, heartbeats], 'ro');
+    end
 end
 
-function [flatcutoffs] = BinarySearch(data, minlength, startmin, endmin)
-    npts = 20;
+function [flatcutoffs] = BinarySearch(data, minlength)
+    npts = 50;
     sample_rate = 1000;
     minbeats = length(data) / sample_rate * 10 / 60; %10 bpm
     maxbeats = length(data) / sample_rate * 150 / 60; %200bpm
+    
     if nargin == 2
         ths = linspace(min(data), max(data), npts);
         beats = zeros(1,npts);
@@ -77,6 +91,7 @@ function [flatcutoffs] = BinarySearch(data, minlength, startmin, endmin)
         beats(1) = CountPeaks(data > ths(1), minlength);
         beats(end) = CountPeaks(data > ths(end), minlength);
     end
+   
    
     for i=1:3
         beats = zeros(1,npts);
@@ -95,7 +110,7 @@ function [flatcutoffs] = BinarySearch(data, minlength, startmin, endmin)
         
         beats = beats(first_valid:last_valid);
         ths = ths(first_valid:last_valid);
-        derivs = abs(beats(2:end)-beats(1:(end-1)));
+        derivs = abs(beats(2:end)-beats(1:(end-1))./ths(2:end)-ths(1:(end-1)));
         %derivs = abs(beats(2:last_valid)-beats(1:(last_valid-1)));
         [minder, idx] = min(derivs);
         if minder == 0
