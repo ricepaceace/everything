ae5e88e906368d643f6e357511eb7d12b2e10c83
Philip Taffet <ptaffet@rice.edu>
2016-12-02 04:02:15 +0000

    Finished arduinocompat.h so you can actually compile with gcc and run on a computer.

--- Arduino/arduinocompat.h
+++ Arduino/arduinocompat.h
@@ -7,5 +7,29 @@
 #define max(a, b) ((a) > (b) ? (a) : (b))
 #define abs(x) ((x) < 0 ? -(x) : x)
 #include <stdbool.h>
+
+#define pinMode(pin, mode) 
+#define boolean bool
+#include <stdio.h>
+
+class _Serial {
+	public:
+	void begin(int a) {}
+	void print(const char* c) { printf("%s", c);}
+	void print(int g) { printf("%d", g); } 
+	void println(const char* c) { printf("%s\n", c);}
+	void println(int g) { printf("%d\n", g); } 
+};
+_Serial Serial;
+
+void setup();
+void loop();
+
+int main(int argc, char** argv)
+{
+	setup();
+	while (true)
+		loop();
+}
 #endif
 
