b955f99deb90469fc9d2268682d52176e8df19cb
Your Name <you@example.com>
2017-02-28 11:45:07 -0600

    moves files around

--- xilnx_design/cycle3/src/sdk/maximDeviceSpecificUtilities.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAXIMDEVICESPECIFICUTILITIES_H_
-#define MAXIMDEVICESPECIFICUTILITIES_H_
-
-u32 start_sampling(u32 unSampleSize, int nSampleRate, u16 *auSamples);
-void continuous_sampling(int k);
-
-#endif
